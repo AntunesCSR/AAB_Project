24258c4957167652949e4ac60a3aa3cacbc2e022
Catia Antunes <antunes.csr@gmail.com>
2023-05-08 01:05:33 +0100

    All scripts corrected and commented

--- Exercise_1/FST_&_Permutations.R
+++ Exercise_1/FST_&_Permutations.R
@@ -4,8 +4,14 @@
 # Date: 25-04-2023
 
 ##########################################################################################################################
+
+# Load packages
+
+library(RColorBrewer)
 source("utilfunctions.r")
-library(hierfstat)
+
+getwd()
+setwd("C:/Users/antun/OneDrive/Documents/Education/MRes/Year 1 - Semester 2/AAB/Final Project/AAB_Project/Exercise_1") # set working directory
 
 ##########################################################################################################################
 
@@ -14,42 +20,35 @@ library(hierfstat)
 # from zero. This function takes as arguments two genotype matrices, and
 # the number of permutations. The output is the p value.
 
-# function that performs a permutation test 
-permutation_test <- function(matrix1, matrix2, n_permutations) {
-  # create a matrix of genotypes by combining the two matrices of genotypes so that the columns represent the SNPs and the rows represent the individuals
-  genotypes <- cbind(matrix1, matrix2)
+permutation_test <- function(geno1, geno2, nperm) {
   
-  # calculate the observed FST value for the populations using the getFst() function from the utilfunctions.r script
-  fst_observed <- getFst(matrix1, matrix2)
+  # combine the genotype matrices
+  geno12 <- cbind(geno1, geno2)
+
+  # get the number of individuals in each population
+  n1 <- ncol(geno1)
+  n2 <- ncol(geno2)
+
+  # compute the observed FST
+  obs_fst <- getFst(geno1, geno2)
+
+  # create a vector to store the permuted FST values
+  perm_fst <- numeric(nperm)
   
-  # create an empty vector with the length of the number of permutations to store the FST values from the permutations
-  fst_permutations <- numeric(n_permutations)
-  
-  # loop through the specified number of permutations
-  for (i in 1:n_permutations) {
-    
-    # randomly permute the indices of the individuals using the sample() function so that the individuals are randomly assigned to populations
-    permuted_indices <- sample(1:ncol(genotypes))
-    
-    # split the permuted indices into two groups of indices, one for each population 
-    permuted_indices1 <- permuted_indices[1:(ncol(matrix1))] # the first half of the permuted indices
-    permuted_indices2 <- permuted_indices[(ncol(matrix1)+1):ncol(genotypes)] # the second half of the permuted indices
-    
-    # create permuted matrices of genotypes by selecting the columns of the original matrices using the permuted indices
-    permuted_matrix1 <- matrix1[, permuted_indices1] # ???
-    permuted_matrix2 <- matrix2[, permuted_indices2] # ???
-    
-    # combine the permuted matrices of genotypes into a single matrix so that the columns represent the SNPs and the rows represent the individuals
-    #permuted_genotypes <- cbind(permuted_matrix1, permuted_matrix2)
-    
-    # calculate the FST for the permuted genotypes using the fst() function from the hierfstat package 
-    fst_permutations[i] <- getFst(permuted_matrix1, permuted_matrix2)
+  # go through each permutation
+  for (i in 1:nperm) {
+    # permute the index of individuals between populations
+    perm_inds <- sample.int(n1 + n2, n1 + n2, replace = FALSE)
+    # get the permuted genotype matrices
+    perm_geno1 <- geno12[, perm_inds[1:n1]] 
+    perm_geno2 <- geno12[, perm_inds[(n1+1):(n1+n2)]]
+    # compute the permuted FST
+    perm_fst[i] <- getFst(perm_geno1, perm_geno2)
   }
-  
-  # calculate the p-value as the proportion of permuted FST values that are greater than the observed FST
-  p_value <- sum(fst_permutations >= fst_observed) / n_permutations
-  
-  return(p_value)
+
+  # compute the p-value
+  pval <- sum(perm_fst >= obs_fst) / nperm
+  return(pval)
 }
 
 ##########################################################################################################################
@@ -58,99 +57,46 @@ permutation_test <- function(matrix1, matrix2, n_permutations) {
 # between all pairwise comparisons for the dataset of Henn et al. (2015), using 1000 permutations, 
 # and a significance level of 0.05.
 
-# import hierfstat package
-library(hierfstat)
-
 # read the genotype matrix
-genotype <- as.matrix(read.table("./AAB_Project/Exercise_1/Hennetal_genotypeMatrix.geno", header=T, stringsAsFactors = F, na.strings = "NA"))
+geno <- as.matrix(read.table("./Hennetal_genotypeMatrix.geno", header = TRUE, stringsAsFactors = FALSE, na.strings = "NA"))
 
-# Get the number of individuals
-nr_ind <- ncol(genotype)
-nr_ind #54
+# read a file with information about individuals and corresponding population
+indpop <- read.table("./IndPopID.txt", header = TRUE, stringsAsFactors = FALSE)
 
-# Get the number of SNPs
-nr_snps <- nrow(genotype)
-nr_snps # 10000
+# Get a vector with the index of population for each individual
+popnames <- unique(indpop$popID)
+popnames
+popinds <- match(indpop$popID, popnames)
+popinds
 
-# Get the label of all individuals
-ind <- colnames(genotype)
-ind
+# Get a list with the index of individuals that belong to each population
+sample_inds <- split(1:ncol(geno), popinds)
+sample_inds
 
-# Read the file that contains the information regarding individuals and corresponding population
-ind_pop <- read.table("./AAB_Project/Exercise_1/IndPopID.txt", header=T, stringsAsFactors = F, na.strings = "NA")
-ind_pop
+# Initialize matrix to save pairwise FST
+pairfst <- matrix(NA, nrow = length(popnames), ncol = length(popnames))
 
-# Get the population labels
-pop_names <- unique(ind_pop$popID)
-pop_names
-length(pop_names) #7
-
-# Get a vector with the index of each individual according to the population
-pop_indx <- numeric(nrow(ind_pop)) # initialize variable
-for(individual in 1:length(ind)) { # loop through each individual in geno matrix
-  # get the index of the line in indpop matrix
-  # that corresponds to the name of ind
-  row_individual <- which(ind_pop$IndID==ind[individual])
-  # get the population of individual ind
-  pop_of_ind <- ind_pop$popID[row_individual]
-  # get the index of the population 
-  pop_indx[individual] <- which(pop_names==pop_of_ind)
-}
-pop_indx # validated pop_indx[20] gives the population of individual 20
-
-# Get a list with the index of individuals grouped by population
-# using for loops
-sample_indx <- list() 
-for(population in 1:length(pop_names)) { # loop through pop.names
-  # apply the function to detect which individuals belong to each pop
-  sample_indx[[population]] <- which(pop_indx==population)
-}
-sample_indx #validated sample_indx[7] gives all the individuals from population 7
-sample_indx[1]
-
-
-#######
-
-# 3. Implementation
-
-# create a list to store the genotype matrices for each population
-populations <- list()
-
-# Split the genotype matrix into population genotype matrices, 1 matrix per population based on sample index
-for (i in 1:length(pop_names)) {
-  populations[[i]] <- genotype[, sample_indx[[i]]]
-}
-length(populations) #7
-
-populations[1]
-
-
-# # Compute the pairwise FST
-# pairfst <- matrix(NA, ncol=length(pop_names), nrow=length(pop_names))
-# # go through each pair of populations
-# for(i in 1:(length(pop_names)-1)) {
-#   for(j in (i+1):length(pop_names)) {
-#     # call the function to compute the pairwise FST between each pair of populations
-#     pairfst[i,j] <-  getFst(genotype[,sample_indx[[i]]],genotype[,sample_indx[[j]]])       
-#   }
-# }
-
-library(hierfstat)
-
-
-# define all pairwise comparisons
-comparisons <- combn(length(populations), 2)
-
-# loop through pairwise comparisons and calculate p-value
-for (i in seq_len(ncol(comparisons))) {
-  matrix1 <- populations[[comparisons[1,i]]]
-  matrix2 <- populations[[comparisons[2,i]]]
-  p_value <- permutation_test(matrix1, matrix2, n_permutations=1000)
-  if (p_value < 0.05) {
-    print(paste0("Pairwise FST between populations ", comparisons[1,i], " and ", comparisons[2,i], " is significant (p-value = ", round(p_value, 4), ")"))
-  } else {
-    print(paste0("Pairwise FST between populations ", comparisons[1,i], " and ", comparisons[2,i], " is not significant (p-value = ", round(p_value, 4), ")"))
+# go through each pair of populations and compute the pairwise FST
+for (i in 1:(length(popnames) - 1)) {
+  for (j in (i + 1):length(popnames)) {
+    # call the function to compute the pairwise FST between each pair of populations
+    pairfst[i, j] <- permutation_test(geno[, sample_inds[[i]]], geno[, sample_inds[[j]]], nperm = 1000)
+    pairfst[j, i] <- pairfst[i, j]
   }
 }
 
-# keep getting error Error in matrix1[, permuted_indices1] : subscript out of bounds
+# Print and export the matrix with the pairwise FST values
+colnames(pairfst) <- popnames
+rownames(pairfst) <- popnames
+pairfst
+write.csv(pairfst, file = "./pairwiseFST.csv", quote = FALSE)
+
+# Print the matrix with the pairwise p-values
+colnames(pairpvalues) <- popnames
+rownames(pairpvalues) <- popnames
+pairpvalues
+
+# Plot and save the pairwise FST values
+png("Pairwise_FST.png", width = 800, height = 800)
+plotFst(pairfst, popnames)
+dev.off()
